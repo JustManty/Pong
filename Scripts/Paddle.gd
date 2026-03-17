42109118ddeed511c711a17ca773930b02cb197b
Manty <tjroberts95@gmail.com>
2026-03-17 19:35:14 -0400

    Add Paddle Movement and World Boundaries

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,29 +1,20 @@
 extends CharacterBody2D
 
-@export var move_speed : int = 5
-var height : int
-var mid_height : int
+@export_range(0, 2) var player : int
+var move_speed : int = 250
 
 func _ready() -> void:
-	height = $Sprite2D.get_rect().size.y
-	mid_height = height / 2
+	pass
 
-func _process(delta: float) -> void:
-	if Input.is_action_pressed("p1_paddle_up"):
-		move_up()
-		pass
-	elif Input.is_action_pressed("p1_paddle_down"):
-		move_down()
-		pass
-
-func move_up():
-	if position.y > mid_height:
-		position.y -= min(position.y - mid_height, move_speed)
+func _physics_process(delta: float) -> void:
+	# Handle Vertical Movement:
+	if Input.is_action_pressed("p" + str(player) + "_paddle_up"):
+		# Set Upward Movement
+		velocity = Vector2(0, -1 * move_speed)
+	elif Input.is_action_pressed("p" + str(player) + "_paddle_down"):
+		# Set Downward Movement
+		velocity = Vector2(0, move_speed)
 	else:
-		position.y = mid_height
-	
-func move_down():
-	if position.y < mid_height:
-		position.y += min(position.y - mid_height + get_viewport_rect().size.y, move_speed)
-	else:
-		position.y = mid_height
+		# Eliminate Movement
+		velocity = Vector2.ZERO
+	move_and_slide()
