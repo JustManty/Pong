b49532026ffdfd70456b66339e8c755683ece34c
Manty <tjroberts95@gmail.com>
2026-03-21 12:39:22 -0400

    Add AI

--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -1,20 +1,34 @@
 extends CharacterBody2D
 
 @export_range(0, 2) var player : int
+@export var use_ai : bool = false
+
+@onready var ball: CharacterBody2D = $"../Ball"
+
 var move_speed : int = 250
 
-func _ready() -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
-	# Handle Vertical Movement:
-	if Input.is_action_pressed("p" + str(player) + "_paddle_up"):
-		# Set Upward Movement
-		velocity = Vector2(0, -1 * move_speed)
-	elif Input.is_action_pressed("p" + str(player) + "_paddle_down"):
-		# Set Downward Movement
-		velocity = Vector2(0, move_speed)
+	if(not use_ai):
+		# Handle Vertical Movement:
+		if Input.is_action_pressed("p" + str(player) + "_paddle_up"):
+			# Set Upward Movement
+			velocity = Vector2(0, -1 * move_speed)
+		elif Input.is_action_pressed("p" + str(player) + "_paddle_down"):
+			# Set Downward Movement
+			velocity = Vector2(0, move_speed)
+		else:
+			# Eliminate Movement
+			velocity = Vector2.ZERO
 	else:
-		# Eliminate Movement
-		velocity = Vector2.ZERO
+		# Handle Vertical Movement:
+		if ball.position.y < position.y:
+			# Set Upward Movement
+			velocity = Vector2(0, -1 * move_speed)
+		elif ball.position.y > position.y:
+			# Set Downward Movement
+			velocity = Vector2(0, move_speed)
+		else:
+			# Eliminate Movement
+			velocity = Vector2.ZERO
+	
 	move_and_slide()
